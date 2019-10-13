39081d34384b855071243678ff50b787b0ed2063
Andy Ho <atho@smu.edu>
2019-10-13 09:30:06 -0700

    new files

---
+++ Universal_Sentence_Encoder_New.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 20:27:53 2019
+
+@author: andyho
+"""
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import platform; print(platform.platform())
+import sys; print("Python", sys.version)
+import requests; print("Requests", requests.__version__)
+import pandas as pd; print("Pandas", pd.__version__)
+import numpy as np; print("Numpy", np.__version__)
+import nltk; print("NLTK", nltk.__version__)
+import re; print("Re", re.__version__)
+import os
+import nltk; print("nltk", nltk.__version__)
+import spacy; print("spacy", spacy.__version__)
+import unidecode
+import unicodedata
+import pattern; print ("pattern", pattern.__version__)
+import string
+import time
+import pickle
+print (os.environ['CONDA_DEFAULT_ENV'])
+
+## Data Analyst Embeddings
+df = pd.read_csv('DataAnalyst_Corpus.csv')
+print(df.head())
+print(len(df))
+
+tempCorpus = df['Corpus']
+dummy = []
+
+with tf.Graph().as_default():
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  messages = tempCorpus.to_list()
+  output = embed(messages)
+ 
+  with tf.Session() as session:
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    dataAnalyst_embeddings = session.run(output)
+
+## Data Scientist Embeddings
+df = pd.read_csv('DataScientist_Corpus.csv')
+print(df.head())
+print(len(df))
+
+tempCorpus = df['Corpus']
+dummy = []
+
+with tf.Graph().as_default():
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  messages = tempCorpus.to_list()
+  output = embed(messages)
+ 
+  with tf.Session() as session:
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    dataScientist_embeddings = session.run(output)
+
+## Data Engineer Embeddings
+df = pd.read_csv('DataEngineer_Corpus.csv')
+print(df.head())
+print(len(df))
+
+tempCorpus = df['Corpus']
+dummy = []
+
+with tf.Graph().as_default():
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  messages = tempCorpus.to_list()
+  output = embed(messages)
+ 
+  with tf.Session() as session:
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    dataEngineer_embeddings = session.run(output)
+
+## Software Engineer Embeddings
+df = pd.read_csv('SoftwareEngineer_Corpus.csv')
+print(df.head())
+print(len(df))
+
+tempCorpus = df['Corpus']
+dummy = []
+
+with tf.Graph().as_default():
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  messages = tempCorpus.to_list()
+  output = embed(messages)
+ 
+  with tf.Session() as session:
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    softwareEngineer_embeddings = session.run(output)
+    
+## Statistician Embeddings
+df = pd.read_csv('Statistician_Corpus.csv')
+print(df.head())
+print(len(df))
+
+tempCorpus = df['Corpus']
+dummy = []
+
+with tf.Graph().as_default():
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  messages = tempCorpus.to_list()
+  output = embed(messages)
+ 
+  with tf.Session() as session:
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    dataEngineer_embeddings = session.run(output)
+    
+## Database Admin Embeddings
+df = pd.read_csv('Database_Corpus.csv')
+print(df.head())
+print(len(df))
+
+tempCorpus = df['Corpus']
+dummy = []
+
+with tf.Graph().as_default():
+  embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+  messages = tempCorpus.to_list()
+  output = embed(messages)
+ 
+  with tf.Session() as session:
+    session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+    databaseAdmin_embeddings = session.run(output)
+
+
+'''
+# Add Feature Vectors to dataframe
+df['Feature_Vector'] = message_embeddings
+print(dummy)
+print(df)
+
+# Export dataframe as pickle
+temp.to_pickle('my_df.pickle')
+'''
